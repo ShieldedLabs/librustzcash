7a150ad158bba31091aab325e4dd8a60162010c7
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2024-06-27 16:18:08 +0200

    [ZIP-233] Add burn_amount to transaction fields

--- zcash_client_sqlite/src/wallet/init.rs
+++ zcash_client_sqlite/src/wallet/init.rs
@@ -849,6 +849,10 @@ mod tests {
                 None,
                 None,
                 None,
+                #[cfg(zcash_unstable = "nsm")]
+                None,
+                #[cfg(zcash_unstable = "tze")]
+                None,
             )
             .freeze()
             .unwrap();
