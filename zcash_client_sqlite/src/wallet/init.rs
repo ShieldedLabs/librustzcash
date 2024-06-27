3afad20db8c738e80dc4d53ae70337f8a8448b39
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2024-06-27 16:18:08 +0200

    added ZSF balance to transactions

--- zcash_client_sqlite/src/wallet/init.rs
+++ zcash_client_sqlite/src/wallet/init.rs
@@ -814,6 +814,10 @@ mod tests {
                 None,
                 None,
                 None,
+                #[cfg(zcash_unstable = "zsf")]
+                None,
+                #[cfg(zcash_unstable = "tze")]
+                None,
             )
             .freeze()
             .unwrap();
